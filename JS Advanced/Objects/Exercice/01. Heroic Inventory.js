f4246796400a8743e1bd415eccb0e332aa01b75e
RadoslavPavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-09-23 16:44:31 +0300

    Exercise: Objects

--- JS Advanced/Objects/Exercice/01. Heroic Inventory.js
+++ JS Advanced/Objects/Exercice/01. Heroic Inventory.js
@@ -1,4 +1,17 @@
 function heroicInventory(params) {
-    
+    let heroes = [];
+    let hero = {};
+    for (const line of params) {
+        let [name, level, items] = line.split(" / ");
+        items = items ? items.split(", ") : items = [];
+        hero.name = name;
+        hero.level = Number(level);
+        hero.items = items;
+        heroes.push(hero);
+        hero = {};
+    }
+    return JSON.stringify(heroes);
 }
-console.log(heroicInventory(params));
+console.log(heroicInventory(['Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara']));
