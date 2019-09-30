504c1a17b2cef6bc580bd4625ae4cc7e285b00f8
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-09-30 04:22:02 +0300

    Exercise: Arrays

--- JS Advanced/Arrays/Exercise/01. Print Array with Given Delimiter.js
+++ JS Advanced/Arrays/Exercise/01. Print Array with Given Delimiter.js
@@ -1,4 +1,14 @@
-function printArrayWithGivenDelimiter(params) {
-    
+function printArrayWithGivenDelimiter(array) {
+    let delimeter = array.splice(-1).join();
+    return array.join(`${delimeter}`);
 }
-console.log(printArrayWithGivenDelimiter(params));
+console.log(printArrayWithGivenDelimiter(
+    [
+        "One",
+        "Two",
+        "Three",
+        "Four",
+        "Five",
+        "-"
+    ]
+));
