c9e9017994c18a5ccf3429e3f6866b33269ad400
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-10-06 22:13:56 +0300

    Exercise: DOM

--- JS Advanced/DOM/Exercise/01. Chat-Room/solution.js
+++ JS Advanced/DOM/Exercise/01. Chat-Room/solution.js
@@ -1,5 +1,18 @@
 function solve() {
-   //TODO...
+   const sendButton = document.getElementById('send'); 
+   const messageContainer = document.getElementById('chat_input');
+
+   sendButton.addEventListener('click', sendMessage);
+
+   function sendMessage() {
+      let message = messageContainer.value;
+
+      let newMessage = document.createElement('div');
+      newMessage.classList.add('message', 'my-message');
+      newMessage.textContent = message;
+
+      document.getElementById('chat_messages').appendChild(newMessage);
+
+      messageContainer.value = '';
+   }
 }
-
-
