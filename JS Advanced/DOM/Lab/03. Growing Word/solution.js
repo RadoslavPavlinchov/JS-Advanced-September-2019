7146a19763ae881ea9c0eefdf7c110bdfa406935
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-10-06 18:27:43 +0300

    Lab: DOM

--- JS Advanced/DOM/Lab/03. Growing Word/solution.js
+++ JS Advanced/DOM/Lab/03. Growing Word/solution.js
@@ -1,4 +1,18 @@
 function growingWord() {
+  const colorMap = ["blue", "green", "red"];
+  
+  const text = document.querySelector('#exercise p');
+  let size = text.style.fontSize.slice(0, -2) * 2;
+  const color = text.style.color;
 
-  //TODO...
+  if (size === 0) {
+    size = 2;
+  }
+
+  console.log(size);
+  const index = colorMap.indexOf(color);
+
+  const nextColor = index + 1 >= colorMap.length ? colorMap[0] : colorMap[index + 1];
+
+  text.style.cssText = `color: ${nextColor}; font-size: ${size}px`;
 }
