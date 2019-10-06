e7aa60b6d2165147847a2ea9720d8ba2c3a7fb14
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-10-06 18:36:10 +0300

    Lab: DOM

--- JS Advanced/DOM/Lab/04. Visited Websites/solution.js
+++ JS Advanced/DOM/Lab/04. Visited Websites/solution.js
@@ -1,4 +1,11 @@
 function solve() {
+  [ ...document.querySelectorAll('.link-1 a') ]
+      .forEach(e => e.addEventListener('click', changeVisited));
 
-  //TODO...
+  function changeVisited(ev) {
+      const curr = ev.currentTarget.nextElementSibling;
+      const arr = curr.textContent.split(' ');
+      arr[1]++;
+      curr.textContent = arr.join(' ');
+  }
 }
