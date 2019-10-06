d813f20ebd2b7fa24622d6c671efb2ff197d5986
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-10-06 17:36:19 +0300

    Lab: DOM

--- JS Advanced/DOM/Lab/02. Format the Text/solution.js
+++ JS Advanced/DOM/Lab/02. Format the Text/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById("input");
+  let output = document.getElementById("output");
+
+  let sentences = input.innerHTML
+    .split(".")
+    .filter(x => x !== "");
+
+  for (let i = 0; i < sentences.length; i += 3) {
+    let p = document.createElement("p");
+    let str = "";
+    for (let j = 0; j < 3; j++) {
+
+      if (i + j < sentences.length) {
+        str += sentences[i + j] + ".";
+      }
+
+    }
+    p.innerHTML = str;
+    output.appendChild(p);
+  }
 }
