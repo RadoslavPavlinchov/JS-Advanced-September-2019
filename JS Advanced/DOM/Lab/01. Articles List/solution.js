5e47a3d5a6a4035b20d386fa0fae0fe97d1ef086
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-10-06 16:39:01 +0300

    Lab: DOM

--- JS Advanced/DOM/Lab/01. Articles List/solution.js
+++ JS Advanced/DOM/Lab/01. Articles List/solution.js
@@ -1,4 +1,34 @@
 function createArticle() {
-	
-	//TODO...
+	let title = document.getElementById("createTitle").value;
+	let content = document.getElementById("createContent").value;
+	let section = document.getElementById("articles");
+
+	function create(t, c) {
+		let article = document.createElement("article");
+		let h3 = document.createElement("h3");
+		let p = document.createElement("p");
+
+		article.appendChild(h3);
+		article.appendChild(p);
+
+		h3.textContent = t;
+		p.textContent = c;
+
+		return article;
+	}
+
+	if (title && content) {
+		const newArticle = create(title, content);
+		section.appendChild(newArticle);
+		document.getElementById('createTitle').value = "";
+        document.getElementById('createContent').value = "";
+	}
 }
+
+
+
+    // <div id="createArticle">
+    // <label for="createTitle">Title</label>
+    // <input id="createTitle"/><br>
+    // <label for="createContent">Content</label>
+    // <textarea id="createContent"></textarea>
