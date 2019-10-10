c60b6f5fb27f0e7b340dda25090ccf9e27afcff0
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-10-10 19:09:39 +0300

    Exercise: DOM

--- JS Advanced/DOM/Exercise/06. Table-Search-Engine/solution.js
+++ JS Advanced/DOM/Exercise/06. Table-Search-Engine/solution.js
@@ -1,3 +1,26 @@
 function solve() {
-   //TODO...
+   let input = document
+      .getElementById("searchField");
+
+   let data = Array.from(document
+         .getElementsByTagName("tr"))
+      .splice(2);
+
+   document
+      .getElementById("searchBtn")
+      .addEventListener("click", function () {
+         data.forEach((e) => e.className = "");
+         if (input.value !== "") {
+            for (let el of data) {
+               Array
+                  .from(el.children)
+                  .forEach(x => {
+                     if (x.textContent.toLocaleLowerCase().includes(input.value.toLocaleLowerCase())) {
+                        el.className = 'select';
+                     }
+                  });
+            }
+         }
+      });
+   input.value = "";
 }
