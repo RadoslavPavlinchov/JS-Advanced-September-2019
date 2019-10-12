ad998977c9e280f257c13c0a0b2282e65d2f5e6c
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-10-12 21:47:58 +0300

    Exercise: DOM Manipulations

--- JS Advanced/DOM Manipulations/Exercise/02. Fill-Dropdown/dropdown.js
+++ JS Advanced/DOM Manipulations/Exercise/02. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    const select = document.querySelector('#menu');
+    const itemText = document.querySelector('#newItemText');
+    const itemValue = document.querySelector('#newItemValue');
+
+    const option = document.createElement('option');
+    option.value = itemValue.value;
+    option.textContent = itemText.value;
+
+    select.appendChild(option);
+    itemText.value = '';
+    itemValue.value = '';
 }
