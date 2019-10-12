5602f5b0680e00543eb912fbfddb3d55e920919a
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-10-12 21:08:32 +0300

    Lab: DOM Manipulations

--- JS Advanced/DOM Manipulations/Lab/07. Dynamic-Validation/app.js
+++ JS Advanced/DOM Manipulations/Lab/07. Dynamic-Validation/app.js
@@ -1,3 +1,10 @@
 function validate() {
-    console.log('TODO:...');
+    document.querySelector('#email').addEventListener('change', (ev) => {
+        const pattern = /[a-z]+@[a-z]+.[a-z]+/gi;
+        if (ev.target.value.match(pattern)) {
+            ev.target.classList.remove('error');
+        } else {
+            ev.target.classList.add('error');
+        }
+    });
 }
