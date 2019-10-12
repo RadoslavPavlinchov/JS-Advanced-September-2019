935414608b78ed03938334f3f7e8bb566449a9af
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-10-12 22:29:27 +0300

    Exercise: DOM Manipulations

--- JS Advanced/DOM Manipulations/Exercise/04. Sections/script.js
+++ JS Advanced/DOM Manipulations/Exercise/04. Sections/script.js
@@ -1,3 +1,21 @@
 function create(words) {
-   console.log('TODO:...');
+   const content = document.querySelector('#content');
+
+   function createDiv(value) {
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+      p.style.display = 'none';
+      p.textContent = value;
+      div.appendChild(p);
+      return div;
+   }
+
+   words.forEach(e => {
+      content.appendChild(createDiv(e));
+   });
+
+   [...document.querySelectorAll('div')]
+   .forEach(e => e.addEventListener('click', (ev) => {
+      ev.target.children[0].style.display = 'block';
+   }));
 }
