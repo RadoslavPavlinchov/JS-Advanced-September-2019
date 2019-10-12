77c8bd3c6ca00c80c62658be8dd0359f13f90fda
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-10-12 20:18:03 +0300

    Lab: DOM Manipulations

--- JS Advanced/DOM Manipulations/Lab/03. Delete-From-Table/app.js
+++ JS Advanced/DOM Manipulations/Lab/03. Delete-From-Table/app.js
@@ -1,3 +1,14 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const result = document.querySelector('#result');
+    const input = document.querySelector('input');
+    const found = Array.from(document.querySelectorAll('tbody tr')).find(e => e.children[1].innerHTML === input.value);
+
+    if (found) {
+        found.parentElement.removeChild(found);
+        result.innerHTML = "Deleted.";
+        input.value = '';
+    } else {
+        result.innerHTML = "Not found.";
+        input.value = '';
+    }
 }
