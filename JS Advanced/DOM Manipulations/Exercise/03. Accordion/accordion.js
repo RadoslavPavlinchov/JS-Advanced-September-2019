2b6c9fcf7aaf4e28443eb22cf6c7fe42700b15a0
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-10-12 21:57:31 +0300

    03. Accordion

--- JS Advanced/DOM Manipulations/Exercise/03. Accordion/accordion.js
+++ JS Advanced/DOM Manipulations/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    const content = document.querySelector('#extra');
+    const [ span ] = document.querySelectorAll('span');
+
+    if (span.textContent === 'More') {
+        span.textContent = 'Less';
+        content.style.display = 'block';
+    } else {
+        span.textContent = 'More';
+        content.style.display = 'none';
+    }
 }
