5f20a0bad2e69cca8eceebb62e83209728f8de97
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-10-12 18:56:22 +0300

    Lab: DOM Manipulations

--- JS Advanced/DOM Manipulations/Lab/02. Add-Delete/app.js
+++ JS Advanced/DOM Manipulations/Lab/02. Add-Delete/app.js
@@ -1,3 +1,29 @@
 function addItem() {
-    //TODO...
+    const input = document.querySelector('#newText');
+
+    function eventDelete(ev){
+        ev.target.parentElement.parentElement.removeChild(ev.target.parentElement);
+    }
+
+    function createElement(type, content, attr) {
+        const el = document.createElement(type);
+        el.innerHTML = content;
+        if (attr !== undefined) {
+            el[attr.name] = attr.value;
+        }
+        return el;
+    }
+
+    function appendElements(parent, child) {
+        return parent.appendChild(child).parentElement;
+    }
+
+    const a = createElement('a', '[Delete]', { name: 'href', value: '#' });
+    const li = createElement('li', input.value);
+    const ul = document.querySelector('#items');
+
+    a.addEventListener('click', eventDelete);
+    ul.appendChild(appendElements(li, a));
+
+    input.value = '';
 }
