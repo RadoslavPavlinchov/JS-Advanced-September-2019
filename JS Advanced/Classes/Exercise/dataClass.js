57507638a9f9047acf7801a929b780801aeba170
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-10-13 11:47:26 +0300

    Exercise: Classes

--- JS Advanced/Classes/Exercise/dataClass.js
+++ JS Advanced/Classes/Exercise/dataClass.js
@@ -1,4 +1,15 @@
 function dataClass() {
-    
+    class Request {
+        constructor(method, uri, version, message) {
+            this.method = method;
+            this.uri = uri;
+            this.version = version;
+            this.message = message;
+            this.response = undefined;
+            this.fulfilled = false;
+        }
+    }
+    let myData = new Request('GET', 'http://google.com', 'HTTP/1.1', "");
+    console.log(myData);
 }
-console.log(dataClass());
+dataClass();
