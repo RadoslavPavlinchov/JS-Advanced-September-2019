7de86528f2f55dca4d72d6470035d64613231181
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-10-13 09:24:47 +0300

    Exercise: DOM Manipulations

--- JS Advanced/DOM Manipulations/Exercise/08. Encode-and-Decode-Messages/app.js
+++ JS Advanced/DOM Manipulations/Exercise/08. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,26 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const sendInput = document.querySelectorAll('textarea')[0];
+    const receiveInput = document.querySelectorAll('textarea')[1];
+
+    const encodeBtn = document.querySelectorAll('button')[0];
+    const decodeBtn = document.querySelectorAll('button')[1];
+
+    function encode() {
+        receiveInput.value = sendInput.value
+        .split('')
+        .map(char => char.charCodeAt() + 1)
+        .map(code => String.fromCharCode(code))
+        .join('');
+        sendInput.value = ''; 
+    }
+
+    function decode() {
+        receiveInput.value = receiveInput.value
+        .split('')
+        .map(char => char.charCodeAt() - 1)
+        .map(code => String.fromCharCode(code))
+        .join('');
+    }
+    encodeBtn.addEventListener("click", encode);
+    decodeBtn.addEventListener('click', decode);
 }
