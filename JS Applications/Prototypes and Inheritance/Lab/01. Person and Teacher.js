ce55c4008503aa712d6504598d8d9c22d1f1ea36
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-11-06 20:40:01 +0200

    Lab: Prototypes and Inheritance

--- JS Applications/Prototypes and Inheritance/Lab/01. Person and Teacher.js
+++ JS Applications/Prototypes and Inheritance/Lab/01. Person and Teacher.js
@@ -1,3 +1,24 @@
-function solve(params) {}
+/* eslint-disable max-classes-per-file */
+function personAndTeacher() {
+    class Person {
+        name;
+        email;
+        constructor(n, e) {
+            this.name = n;
+            this.email = e;
+        }
+    }
+    class Teacher extends Person {
+        subject;
+        constructor(n, e, s) {
+            super(n, e);
+            this.subject = s;
+        }
+    }
 
-solve();
+    return {
+        Person,
+        Teacher,
+    }
+}
+personAndTeacher()
