3883a616532234b9a5dfeabcb72bb755567621a4
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-11-10 15:39:28 +0200

    Rest Services and AJAX

--- JS Applications/Rest Services and AJAX/Lab/05.Github-Repos/scripts/app.js
+++ JS Applications/Rest Services and AJAX/Lab/05.Github-Repos/scripts/app.js
@@ -1,3 +1,29 @@
 function loadRepos() {
-	console.log("TODO...");
+    const name = document.querySelector('#username').value;
+    const list = document.querySelector('#repos');
+    const url = `https://api.github.com/users/${name}/repos`;
+
+    list.removeChild(list.firstElementChild);
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(e => {
+                const li = document.createElement('li');
+                const a = document.createElement('a');
+
+                a.href = e.html_url;
+                a.textContent = e.full_name;
+
+                li.appendChild(a);
+                list.appendChild(li);
+            });
+        })
+        .catch(error => {
+            const li = document.createElement('li');
+            li.textContent = error;
+            list.appendChild(li);
+        });
+
+    list.textContent = '';
 }
