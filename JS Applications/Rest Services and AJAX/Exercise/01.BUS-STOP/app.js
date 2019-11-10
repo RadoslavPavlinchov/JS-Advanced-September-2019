3cf1f034779700461656b8c7d2a298444e5e0e87
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-11-10 17:49:59 +0200

    Rest Services and AJAX

--- JS Applications/Rest Services and AJAX/Exercise/01.BUS-STOP/app.js
+++ JS Applications/Rest Services and AJAX/Exercise/01.BUS-STOP/app.js
@@ -1,3 +1,29 @@
 function getInfo() {
-    console.log("TODO...");
+    const stopName = document.querySelector('#stopName');
+    const busesList = document.querySelector('#buses');
+    const stopId = document.querySelector('#stopId').value;
+    const url = `https://judgetests.firebaseio.com/businfo/${stopId}.json`;
+
+    fetch(url)
+        .then(res => {
+            if (!res.ok) {
+                stopName.textContent = 'Error';
+                throw new Error(
+                    `Unable to find a bus stop with this Stop ID: ${stopId}`
+                );
+            }
+            return res.json();
+        })
+        .then(data => {
+            const { name, buses } = data;
+            stopName.textContent = name;
+            Object.entries(buses).forEach(([id, time]) => {
+                const li = document.createElement('li');
+                li.textContent = `Bus ${id} arrives in ${time}`;
+                busesList.appendChild(li);
+            });
+        });
+
+    stopName.textContent = '';
+    busesList.innerHTML = '';
 }
