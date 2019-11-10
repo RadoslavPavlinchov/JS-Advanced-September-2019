d0387797ad97579e9676ed1eb1b9d417160b73b4
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-11-10 22:43:33 +0200

    Rest Services and AJAX

--- JS Applications/Rest Services and AJAX/Exercise/02.BUS-SCHEDULE/app.js
+++ JS Applications/Rest Services and AJAX/Exercise/02.BUS-SCHEDULE/app.js
@@ -1,11 +1,34 @@
 function solve() {
+    const infoBox = document.querySelector('.info');
+    const departBtn = document.querySelector('#depart');
+    const arriveBtn = document.querySelector('#arrive');
+
+    let currentStop = 'depot';
+    let arrivingAt;
 
     function depart() {
-        console.log('Depart TODO...');
+        fetch(`https://judgetests.firebaseio.com/schedule/${currentStop}.json`)
+            .then(res => res.json())
+            .then(data => {
+                let { name, next } = data;
+                currentStop = next;
+                arrivingAt = name;
+                infoBox.textContent = `Next stop ${name}`;
+
+                departBtn.disabled = true;
+                arriveBtn.disabled = false;
+            })
+            .catch(err => {
+                infoBox.textContent = 'Error'
+                departBtn.disabled = true;
+                arriveBtn.disabled = true;
+            });
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoBox.textContent = `Arriving at ${arrivingAt}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
