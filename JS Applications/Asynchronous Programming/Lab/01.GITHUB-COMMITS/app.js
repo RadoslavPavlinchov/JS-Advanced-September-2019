741323286cf5ba06fb49159989b2bcecc556219b
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-11-17 23:26:17 +0200

    Asynchronous Programming

--- JS Applications/Asynchronous Programming/Lab/01.GITHUB-COMMITS/app.js
+++ JS Applications/Asynchronous Programming/Lab/01.GITHUB-COMMITS/app.js
@@ -1,4 +1,40 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+
+    const html = {
+        'username': () => document.getElementById('username'),
+        'repository': () => document.getElementById('repo'),
+    }
+
+    const user = html.username().value;
+    const repo = html.repository().value;
+
+    const url = `https://api.github.com/repos/${user}/${repo}/commits`;
+
+    const ul = document.getElementById('commits');
+
+    fetch(url)
+        .then(response => {
+            if (response.status !== 200) {
+                const li = document.createElement('li');
+                li.textContent = `Error: ${response.status} (${response.statusText})`
+                ul.appendChild(li);
+                return;
+            }
+            return response;
+        })
+        .then(r => r.json())
+        .then(data => {
+            data.forEach(e => {
+                const li = document.createElement('li');
+                li.textContent = `${e.commit.author.name}: ${e.commit.message}`
+                ul.appendChild(li);
+            });
+
+        })
+        .catch(err => err);
+
+    html.username().value = ''
+    html.repository().value = ''
+    ul.innerHTML = '';
+
 }
