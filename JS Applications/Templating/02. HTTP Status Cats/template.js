2997267b5ee92ce337acb29f51597b5a3aa69d33
Radoslav Pavlinchov <46440360+RadoslavPavlinchov@users.noreply.github.com>
2019-11-20 23:13:39 +0200

    Templating

--- JS Applications/Templating/02. HTTP Status Cats/template.js
+++ JS Applications/Templating/02. HTTP Status Cats/template.js
@@ -1,8 +1,21 @@
-(() => {
-     renderCatTemplate();
+function toggle(e) {
+    e.textContent = e.textContent === 'Show status code' ? 'Hide status code' : 'Show status code'
+    const statusDiv = e.parentNode.getElementsByClassName('status')[0]
+    statusDiv.style.display = statusDiv.style.display === 'none' ? 'inline' : 'none';
+}
 
-     function renderCatTemplate() {
-         // TODO: Render cat template and attach events
-     }
- 
-})
+(() => {
+    renderCatTemplate();
+
+    async function renderCatTemplate() {
+        const source = await fetch('http://127.0.0.1:5500/JS%20Applications/Templating/Exercise/02.%20HTTP%20Status%20Cats/templates/all-cats.hbs')
+            .then(res => res.text())
+
+        const template = Handlebars.compile(source);
+        const context = { cats: window.cats };
+        const catsHTML = template(context);
+
+        document.getElementById('allCats')
+            .innerHTML = catsHTML;
+    }
+})()
